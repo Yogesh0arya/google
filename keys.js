992e314b1de5daaed4b83138cf48578c75fcc4d4
Yogesh0arya <yogesh084arya@gmail.com>
2021-04-14 11:57:36 +0530

    app build

---
+++ keys.js
@@ -0,0 +1,5 @@
+//api key - https://developers.google.com/custom-search/v1/using_rest
+export const API_KEY = 'AIzaSyAJttHpVxKPiqMh5G_5DPDfk2_5NURjQDI'
+
+//context key - https://cse.google.com/cse/create/new
+export const CONTEXT_KEY = '043a3e9fe9c20b0d1'
